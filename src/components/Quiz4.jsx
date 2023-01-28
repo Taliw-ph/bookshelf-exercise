f2a2bd1274a12c5b6f00b633f56f993721c3ef38
Phanuwat Phoowichai <phanu_phoo@kkumail.com>
2023-01-28 08:53:14 +0000

    Update 01

--- src/components/Quiz4.jsx
+++ src/components/Quiz4.jsx
@@ -1,1 +1,10 @@
+import Quiz1 from "./Quiz1";
+
 // Write component name Quiz4 and display your name
+const Quiz4 = () => (
+  <p>
+    <b>Taliw Phanuwat</b> for Quiz4
+  </p>
+);
+
+export default Quiz4;
