4772c9fc24d4b8f4f519003df8ecf48a65cecfd5
Phanuwat Phoowichai <taliw@odds.team>
2023-01-29 14:55:36 +0700

    updated 03

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
+'use client';
 import AddBookForm from "@/components/AddBookForm";
 import BookList from "@/components/BookList";
+import { useState } from 'react';
 
 const HomePage = () => {
-  const books = [{ title: "Clean Code" }];
+  const [books, setBooks] = useState([{ title: "Clean Code" }]);
   return (
     <div>
-      <AddBookForm />
+      <AddBookForm setBooks={setBooks} />
       <hr />
       <BookList books={books} />
     </div>
--- src/components/AddBookForm.jsx
+++ src/components/AddBookForm.jsx
@@ -1,21 +1,32 @@
-"use client";
-import { useState } from "react";
+'use client';
+import { useState } from 'react';
 
-const AddBookForm = () => {
-  const [title, setTitle] = useState("Domain Driven Design");
-  return (
-    <div>
-      <h1>ADD TO COLLECTION</h1>
-      <form>
+const AddBookForm = ({ setBooks }) => {
+    const [title, setTitle] = useState('Domain Driven Design');
+
+    const onTitleChange = (e) => setTitle(e.target.value);
+
+    const onBookSubmit = (e) => {
+        if (title === '') return;
+        e.preventDefault();
+        setBooks((prev) => [...prev, { title }]);
+        setTitle('');
+    };
+
+    return (
         <div>
-          <label>Title </label>
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
-          <br />
-          {title}
+            <h1>ADD TO COLLECTION</h1>
+            <form onSubmit={onBookSubmit}>
+                <div>
+                    <label>Title </label>
+                    <input value={title} onChange={onTitleChange} />
+                    <button type="submit">Add</button>
+                    <br />
+                    {title}
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default AddBookForm;
